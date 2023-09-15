a21b22bca62c997591e0a8d9fcc3f6784ff6508f
NikoTristen <106286279+NikoTristen@users.noreply.github.com>
2023-09-14 18:20:28 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+
+
+function formatDate(timestamp){
+
+  let date = new Date();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  if (hour < 10 ){
+    hour = `0${hour}`;
+  }
+  if (minutes < 10 ){
+    minutes = `0${minutes}`;
+  }
+  return `${day} at ${hour}:${minutes}`;
+
+}
+
+function getLiveWeather(response){
+  document.querySelector("#main-temp").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("#weatherDescription").innerHTML = response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
+  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#city").innerHTML = response.data.name;
+
+  document.querySelector("#date").innerHTML = formatDate("timestamp");
+}
+
+
+let units = "imperial"
+let city = "New York";
+let apiKey = "197f9fd906875b61a67bac12da5e6cdb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(getLiveWeather);
